3269b1eb4013738dcbfd825f99aa3142b3d3f242
artem_skynox <pumba777333@mail.ru>
2015-12-03 13:32:44 +0300

    Добавил карту исправил косяки

--- app/src/main/java/com/skynox/whereyou/app/activity/MapActivity.java
+++ app/src/main/java/com/skynox/whereyou/app/activity/MapActivity.java
@@ -1,6 +1,10 @@
 package com.skynox.whereyou.app.activity;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.skynox.whereyou.app.R;
 
 /**
@@ -9,17 +13,27 @@ import com.skynox.whereyou.app.R;
  * Дата: 28.11.2015
  */
 
-public class MapActivity extends AbstractDefaultActivity {
+public class MapActivity extends AbstractDefaultActivity implements OnMapReadyCallback {
 
     private int layout = R.layout.activity_map;
+
+
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapView);
+        if (mapFragment!=null && !mapFragment.isVisible() && !mapFragment.isAdded())
         setContentView(layout);
         initToolbar();
         initNavigationView();
     }
 
+    @Override
+    public void onMapReady(final GoogleMap map) {
+
+    }
 }
